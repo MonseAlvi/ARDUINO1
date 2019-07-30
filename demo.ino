227fddeff5e8dbd8f827a2aa81cca5f07594bf4d
MonseAlvi <fer.alvizuri999@gmail.com>
2019-07-30 13:23:00 -0500

    puntito cada 5 segundos

--- demo.ino
+++ demo.ino
@@ -3,10 +3,20 @@
   
 void setup() {
     // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("inicializar programa2");
+  Serial.println("tengo flojera de programar");
   
   }
-  
+
+
   void loop() {
     // put your main code here, to run repeatedly:
+  
+
+
+   Serial.print(".");
+   delay(5000);
+    
   
   }     
