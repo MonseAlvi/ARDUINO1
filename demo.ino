95065fb3e68bfb0f4773d0bf66c695583e52794c
MonseAlvi <fer.alvizuri999@gmail.com>
2019-07-31 12:03:43 -0500

    pendiente

--- demo.ino
+++ demo.ino
@@ -1,22 +1,26 @@
 
+  int PINOUT = 2;
   
   
 void setup() {
     // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println("inicializar programa2");
-  Serial.println("tengo flojera de programar");
+  Serial.println("inicializar programa");
+  Serial.println("setting pin 2 as output");
+  pinMode(PINOUT, OUTPUT);
+  
   
   }
-
 
   void loop() {
     // put your main code here, to run repeatedly:
   
-
-
-   Serial.print(".");
-   delay(5000);
+   Serial.print("PIN 2 -> ON");
+   digitalWrite(PINOUT, HIGH);
+   delay (500);
+    Serial.print("PIN 2 -> OFF");
+   digitalWrite(PINOUT, LOW);
+    delay (500);
     
   
   }     
